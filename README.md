c8b3c2c8173f67bf93cbdd1cad65e7fba1bd35c4
Dovah Merith <merith.tk@gmail.com>
2019-02-25 15:14:12 -0800

    First Push, basic webveiw enabled

---
+++ README.md
@@ -0,0 +1,9 @@
+I have utilized this guys source example template to make a kotlin version of the Website https://devdocs.io
+THIS WILL NOT BE OFFICIALLY SUPPORTED as it will use a webveiw, not truely offline I know, but it will do. 
+
+##ORIGINAL SOURCE
+https://github.com/QuaestioOrg/kotlin-converted-webview/ast.
+## License
+Licensed under the [Apache License, Version 2.0 (the "License")](http://www.apache.org/licenses/LICENSE-2.0); you may not use files of this sample except in compliance with its [License](/LICENSE).
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "as is" basis, without warranties or conditions of any kind, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
---
+++ app/src/main/java/org/quaestio/kotlinconvertedwebview/MainActivity.kt
@@ -0,0 +1,57 @@
+package org.quaestio.kotlinconvertedwebview
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_webview.*
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_webview)
+
+        val mWebView = findViewById<WebView>(R.id.webview)
+
+        val webSettings = mWebView.settings
+        webSettings.javaScriptEnabled = true
+        mWebView.loadUrl(getString(R.string.website_url))
+        mWebView.webViewClient = HelloWebViewClient()
+        WebView.setWebContentsDebuggingEnabled(false)
+
+    }
+
+    private inner class HelloWebViewClient : WebViewClient() {
+
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            if (Uri.parse(url).host == getString(R.string.website_domain)) {
+                return false
+            }
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+            return true
+        }
+
+        override fun onPageFinished(view: WebView, url: String) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url)
+        }
+
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webview.canGoBack()) {
+            webview.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+}
